2b38bfff15c3c2e3363503621c5016b1cc63c757
Sarthak Bansal <SarthakBansal725@gmail.com>
2024-12-30 10:07:58 +0530

    user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -3,7 +3,7 @@
 
 const asyncHandler = (fn) => async (req,res,next) => {
     try {
-        
+        await fn(req,res,next);
     } catch (error) {
         res.send(err.code || 500).json({
             success: false,
